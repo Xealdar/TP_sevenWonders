4149109f97815ae6e75185c48d35bb410f29567c
David Seroussi <davidseroussi97@gmail.com>
2019-04-04 10:50:38 +0200

    Reset divinity

--- divinity.js
+++ divinity.js
@@ -28,14 +28,6 @@ class Divinity {
         this.worldEvents.emit('retribution', Math.floor(10000 * Math.random()));
       }
     }, this.timeFactor);
-
-
-    this.stockMarketInterval_ = setInterval(() => {
-      let plusOrMinus = Math.random() < 0.5 & this.cornPrice > 1 ? -1 : 1;
-      this.cornPrice_ += plusOrMinus * Math.floor((this.cornPrice_ + 3) * Math.random());
-      if (this.cornPrice <= 0)
-        this.cornPrice_ = 1;
-    }, this.timeFactor * Math.floor(5 * Math.random()));
   }
 
   offeringCorn(offer) {
@@ -68,56 +60,6 @@ class Divinity {
             `You didn't gave a number of gold to ${this.name_}, Earth collapsed`
           )
         );
-      }
-    });
-  }
-
-  buyingCorn(divinity, corn) {
-    return new Promise((resolve, reject) => {
-      if (divinity instanceof Divinity) {
-        setTimeout(() => {
-
-          if (Math.random() > 0.5)
-            return reject(
-              new Error(
-                `Sorry, ${divinity.name} refused to sell corn`
-              )
-            );
-
-          let goldToPay = divinity.cornPrice_ * corn;
-          if (goldToPay <= this.gold) {
-            if (Math.random() > 0.10) {
-              let lostGold = Math.round(Math.random() * goldToPay);
-              let lostCorn = Math.round(Math.random() * corn);
-              
-              divinity.corn_ -= lostCorn;
-              this.gold_ -= lostGold;
-              reject(
-                new Error(
-                  `You were attacked by a wild Maxime Robin ! You lost ${lostGold} gold and ${divinity.name} lost ${lostCorn} corn !`
-                )
-              )
-            } else {
-              this.gold_ -= goldToPay;
-              this.corn_ += corn;
-              divinity.gold_ += goldToPay;
-              divinity.corn_ -= corn;
-              resolve();
-            }
-          } else {
-            reject(
-              new Error(
-                `You need ${goldToPay} gold to buy ${corn} corn at the price of ${divinity.cornPrice} gold/corn. You only have ${this.gold_} gold.`
-              )
-            )
-          }
-        }, 3 * this.timeFactor * Math.random());
-      } else {
-        reject(
-          new Error(
-            'You have to trade with a Divinity'
-          )
-        )
       }
     });
   }
