6ecd621e52a7dbdb2617b76adc9337ed6baf6122
Xealdar <alex.mollaret@gmail.com>
2019-04-04 16:31:35 +0200

    update city class

--- city.js
+++ city.js
@@ -1,16 +1,35 @@
-const EventEmitter = require('events');
+const {Divinity} = require('./divinity');
 
 class City {
-  constructor(name) {
+  constructor(name, divinityName) {
     this.name_ = name || 'UNKCITY';
-    this.corn_ = 5000;
+    this.divinity_ = new Divinity(divinityName);
+    this.corn_ = 1000;
     this.gold_ = 1000;
+    this.init();
   }
 
-  init(){
-    
+  init() {
+    this.divinity_.init();
+    this.divinity_.worldEvents.on('favor', shit => this.getShit(shit));
+    this.divinity_.worldEvents.on('blessing', shit => this.getShit(shit));
   }
 
+  getShit(s) {
+    this.corn_ += Math.floor(s.corn);
+    this.gold_ += Math.floor(s.gold);
+  }
+
+  giveShit() {
+    this.divinity_.offeringCorn(this.corn_);
+    this.divinity_.offeringGold(this.gold_);
+    this.corn_ = 0;
+    this.gold_ = 0;
+  }
+
+  showShit() {
+    console.log(`${this.name_}: C ${this.corn_}, G ${this.gold_}`);
+  }
 }
 
-module.exports = {City}
+module.exports = {City};
