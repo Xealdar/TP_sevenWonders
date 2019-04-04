452a8bdd44c2201e32a45cdf494b0f32a8d493f3
David Seroussi <davidseroussi97@gmail.com>
2019-04-04 17:13:28 +0200

    Added troop

---
+++ troop.js
@@ -0,0 +1,42 @@
+class Troop {
+
+    constructor(id, age, price, life) {
+        this.id = id;
+        this.age = age;
+        this.price = price;
+        this.life = life;
+    }
+
+
+    init() {
+
+    }
+
+    fight() {
+
+    }
+
+
+    defense() {
+
+    }
+
+    get id() {
+        return this.id;
+    }
+
+    get age() {
+        return this.age;
+    }
+
+
+    get price() {
+        return this.price;
+    }
+
+    get life() {
+        return this.life;
+    }
+}
+
+module.exports = { Troop }
